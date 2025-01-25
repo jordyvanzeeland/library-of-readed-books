30b60543ee56e57316992700289783eceb9d741b
Jordy van Zeeland <jordyvanzeeland@gmail.com>
2025-01-25 15:37:01 +0100

    Added not found page

--- controller/PageController.php
+++ controller/PageController.php
@@ -4,14 +4,16 @@
     
     class PageController{
         public $currentUser;
+        public $viewsfolder;
 
         public function __construct(){
             $this->currentUser = (new AuthController())->getCurrentUser();
+            $this->viewsfolder = dirname(__DIR__, 1) . "/views/";
         }
 
         public function renderTemplate(string $tmpname, array $args = []){
             extract($args, EXTR_SKIP);
-            require(dirname(__DIR__, 1) . "/views/" . "{$tmpname}.view.php");
+            require($this->viewsfolder . "{$tmpname}.view.php");
         }
 
         public function getView(string $view, array $args = []){
@@ -19,7 +21,11 @@
                 $this->renderTemplate('login', $args);
             }else{
                 if(isset($view) && $view !== '' && $view !== '?logout=1'){
-                    $this->renderTemplate($view, $args);
+                    if(!file_exists($this->viewsfolder . "{$view}.view.php")){
+                        $this->renderTemplate('notfound', $args);
+                    }else{
+                        $this->renderTemplate($view, $args);
+                    }
                 }else if(!isset($view) || isset($view) && $view == ""){
                     $this->renderTemplate('home', $args);
                 }
---
+++ views/notfound.view.php
@@ -0,0 +1,2 @@
+<h1>404 - Pagina niet gevonden</h1>
+<p>De pagina die je probeert te zoeken is niet gevonden.</p>
